10eeb353b07ec5f2413bb132db41c3b74d59aa10
Adam Cammack <acammack42@gmail.com>
2015-10-16 21:30:20 -0500

    Quote ALL THE THINGS!!
    Also some workflow changes

--- whatnext-ac.sh
+++ whatnext-ac.sh
@@ -12,22 +12,22 @@ _whatnext() {
 
   case $COMP_CWORD in
     1)
-      COMPREPLY=( $( compgen -W 'add queue start finish list open echo' -- $cur ) )
+      COMPREPLY=( $( compgen -W 'add queue start finish tree ls open echo' -- "$cur" ) )
       ;;
     2)
-      COMPREPLY=( $( compgen -W "$(ls $WN_PROJECTS_DIRECTORY)" -- $cur ) )
+      COMPREPLY=( $( compgen -W "$(ls "$WN_PROJECTS_DIRECTORY")" -- "$cur" ) )
       ;;
     3)
       case "$action" in
         queue)
-          COMPREPLY=( $( compgen -W "$(ls $WN_PROJECTS_DIRECTORY/$project)" -- $cur ) )
+          COMPREPLY=( $( compgen -W "$(ls "$WN_PROJECTS_DIRECTORY/$project")" -- "$cur" ) )
           ;;
         start)
           COMPREPLY=()
           [ -d "$WN_PROJECTS_DIRECTORY/$project" ] &&
-            COMPREPLY=( $( compgen -W "$(ls $WN_PROJECTS_DIRECTORY/$project)" -- $cur ) )
+            COMPREPLY=( $( compgen -W "$(ls "$WN_PROJECTS_DIRECTORY/$project")" -- "$cur" ) )
           [ -d "$WN_ACTIVE_DIRECTORY/$project" ] &&
-            COMPREPLY=( $( compgen -W "$(ls $WN_ACTIVE_DIRECTORY/$project) $(ls $WN_PROJECTS_DIRECTORY/$project)" -- $cur ) )
+            COMPREPLY=( $( compgen -W "$(ls "$WN_ACTIVE_DIRECTORY/$project") $(ls "$WN_PROJECTS_DIRECTORY/$project")" -- "$cur" ) )
           ;;
         *) COMPREPLY=()
       esac
