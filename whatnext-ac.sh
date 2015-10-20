74306ef411c7c9a0547002269bac36ce36549890
Adam Cammack <acammack42@gmail.com>
2015-10-19 19:23:41 -0500

    Use more intelligent file search for projects, too

--- whatnext-ac.sh
+++ whatnext-ac.sh
@@ -2,6 +2,30 @@
 
 source whatnext
 
+# $1: directory
+# $2: -a to include hidden files on empty completion
+_whatnext_file_search() {
+  local cur=${COMP_WORDS[COMP_CWORD]}
+  pushd $1 >/dev/null
+
+  # From the bash-completion package
+  _filedir
+  popd >/dev/null
+
+  if [ ! "-a" == "$2" ]; then
+    if [ ! "." == "${cur:0:1}" ]; then
+      # For some reason ${array[@]//.*/} doesn't work here
+      local x
+      for x in "${!COMPREPLY[@]}"; do
+        local d="${COMPREPLY[$x]}"
+        if [ "." == "${d:0:1}" ]; then
+          unset COMPREPLY[$x]
+        fi
+      done
+    fi
+  fi
+}
+
 _whatnext() {
   local cur action project project_dir active_dir
 
@@ -15,39 +39,20 @@ _whatnext() {
       COMPREPLY=( $( compgen -W 'add queue start finish open ls tree echo' -- "$cur" ) )
       ;;
     2)
-      COMPREPLY=( $( compgen -W "$(ls --quoting-style=escape "$WN_PROJECTS_DIRECTORY")" -- "$cur" ) )
+      _whatnext_file_search $WN_PROJECTS_DIRECTORY
       ;;
     3)
       project_dir="$WN_PROJECTS_DIRECTORY/$project"
       active_dir="$WN_ACTIVE_DIRECTORY/$project"
       case "$action" in
         queue)
-          pushd $project_dir >/dev/null
-          # From the bash-completion package
-          _filedir
-          popd >/dev/null
-          if [ ! "." == "${cur:0:1}" ]; then
-            # For some reason ${array[@]//.*/} doesn't work here
-            local x
-            for x in "${!COMPREPLY[@]}"; do
-              local d="${COMPREPLY[$x]}"
-              if [ "." == "${d:0:1}" ]; then
-                unset COMPREPLY[$x]
-              fi
-            done
-          fi
+          _whatnext_file_search $project_dir
           ;;
         start)
-          pushd $active_dir >/dev/null
-          # From the bash-completion package
-          _filedir
-          popd >/dev/null
+          _whatnext_file_search $active_dir -a
           ;;
         open)
-          pushd $project_dir >/dev/null
-          # From the bash-completion package
-          _filedir
-          popd >/dev/null
+          _whatnext_file_search $project_dir -a
           ;;
         *) COMPREPLY=()
       esac
